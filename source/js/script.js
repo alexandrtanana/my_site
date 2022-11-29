bcffa28c79f1d822cbc814e322867935015b985e
alexandrtanana <tanana1994@list.ru>
2022-11-29 21:18:38 +0300

    Сделал галерею

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,17 @@
-// бургер меню
 
-var navMain = document.querySelector('.page-header__nav');
-var navToggle = document.querySelector('.page-header__toggle');
+// // бургер меню
 
-navMain.classList.remove('page-header__nav--nojs');
+// var navMain = document.querySelector('.page-header__nav');
+// var navToggle = document.querySelector('.page-header__toggle');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('page-header__nav--closed')) {
-    navMain.classList.remove('page-header__nav--closed');
-    navMain.classList.add('page-header__nav--opened');
-  } else {
-    navMain.classList.add('page-header__nav--closed');
-    navMain.classList.remove('page-header__nav--opened');
-  }
-});
+// navMain.classList.remove('page-header__nav--nojs');
 
+// navToggle.addEventListener('click', function () {
+//   if (navMain.classList.contains('page-header__nav--closed')) {
+//     navMain.classList.remove('page-header__nav--closed');
+//     navMain.classList.add('page-header__nav--opened');
+//   } else {
+//     navMain.classList.add('page-header__nav--closed');
+//     navMain.classList.remove('page-header__nav--opened');
+//   }
+// });
